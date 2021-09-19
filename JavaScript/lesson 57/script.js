dd476f1cef4329c73d5de637889019bd6cb61d74
DELL <chynara.tech@gmail.com>
2021-09-19 16:38:41 +0300

    db.json

--- JavaScript/lesson 57/script.js
+++ JavaScript/lesson 57/script.js
@@ -1,111 +1,115 @@
 (function () {
     'use strict';
 
-    const inputSom = document.querySelector("#som"),
-          inputUsd = document.querySelector("#usd");  
+    fetch("db.json")  //обратилась к файлу db.json
+        .then(data => data.json())  // получили сам ответ, сконвертировали его в json
+        .then(res => console.log(res)); // получаем его как результат и выводим
 
-    inputSom.addEventListener('input', () =>{
+//     const inputSom = document.querySelector("#som"),
+//           inputUsd = document.querySelector("#usd");  
+
+//     inputSom.addEventListener('input', () =>{
        
-        const request = new XMLHttpRequest();
+//         const request = new XMLHttpRequest();
 
-        // request.open(method(GET/POST), url, asy, login, password);
-        request.open("GET", "current.json");
-        request.setRequestHeader("Content-type", "application/json; charset-UTF-8");
-        request.send();
+//         // request.open(method(GET/POST), url, asy, login, password);
+//         request.open("GET", "current.json");
+//         request.setRequestHeader("Content-type", "application/json; charset-UTF-8");
+//         request.send();
 
-        // request.addEventListener('readystatechange', () => {
-        request.addEventListener('load', () => {
+//         // request.addEventListener('readystatechange', () => {
+//         request.addEventListener('load', () => {
 
-            // if(request.readyState === 4 && request.status === 200){
-            if(request.status === 200){
-               // console.log(request.response);
-               // console.log(JSON.parse(request.response));
+//             // if(request.readyState === 4 && request.status === 200){
+//             if(request.status === 200){
+//                // console.log(request.response);
+//                // console.log(JSON.parse(request.response));
 
-                const currency = JSON.parse(request.response);
+//                 const currency = JSON.parse(request.response);
 
-                const result = inputSom.value / currency.current.usd;
+//                 const result = inputSom.value / currency.current.usd;
 
-                inputUsd.value = (result).toFixed(2);
-            } else {
-                inputUsd.value ="Что то пошло не так !";
-            }
+//                 inputUsd.value = (result).toFixed(2);
+//             } else {
+//                 inputUsd.value ="Что то пошло не так !";
+//             }
 
-        });
-        // status  - 404
-        // status  - mistake text
-        // response
-        // readyState - отслеживать этапы на котором находимся
-    });
+//         });
+//         // status  - 404
+//         // status  - mistake text
+//         // response
+//         // readyState - отслеживать этапы на котором находимся
+//     });
 
 
-    const search   = document.querySelector("[name='search']"),
-          btnSearch = document.querySelector(".search-form button"),
-         search_res = document.querySelector(".search_result");
+//     const search   = document.querySelector("[name='search']"),
+//           btnSearch = document.querySelector(".search-form button"),
+//          search_res = document.querySelector(".search_result");
             
-    btnSearch.addEventListener('click', (e) => {
-        e.preventDefault();
+//     btnSearch.addEventListener('click', (e) => {
+//         e.preventDefault();
 
 
-        // Отправка данных с помощью Fetch API
+//         // Отправка данных с помощью Fetch API
 
-        fetch('search.php', {
-            method: "POST",
-            body: JSON.stringify({ query: search.value }),
-            headers: {
-                "Content-type": "application/json"
-            }
-        })
-            .then(response => response.json())
-            .then(json => {
-                let creat_ul = document.createElement('ul');
+//         fetch('search.php', {
+//             method: "POST",
+//             body: JSON.stringify({ query: search.value }),
+//             headers: {
+//                 "Content-type": "application/json"
+//             }
+//         })
+//             .then(response => response.json())
+//             .then(json => {
+//                 let creat_ul = document.createElement('ul');
 
-                for (let i = 0; i < json.length; i++) {
+//                 for (let i = 0; i < json.length; i++) {
 
-                    creat_ul.innerHTML += `<li>
-                                            <a href="${json[i].href}">${json[i].text}</a>    
-                                            </li>`;
-                }
+//                     creat_ul.innerHTML += `<li>
+//                                             <a href="${json[i].href}">${json[i].text}</a>    
+//                                             </li>`;
+//                 }
 
-                search_res.innerHTML = creat_ul.outerHTML;
-            })
-            .catch(console.log("Ошибка"));
+//                 search_res.innerHTML = creat_ul.outerHTML;
+//             })
+//             .catch(console.log("Ошибка"));
         
 
-        // Отправка данных с помощью XMLHttpRequest
+//         // Отправка данных с помощью XMLHttpRequest
 
-    //     const request_ = new XMLHttpRequest();
+//     //     const request_ = new XMLHttpRequest();
 
-    //     request_.open("GET", "search.php?query="+search.value);
+//     //     request_.open("GET", "search.php?query="+search.value);
 
-    //     search_res.innerHTML = "Данные отправлены";
-    //     request_.send();
+//     //     search_res.innerHTML = "Данные отправлены";
+//     //     request_.send();
 
-    //     request_.addEventListener('load', () => {
+//     //     request_.addEventListener('load', () => {
 
-    //         if(request_.status ===200){
-    //           
-                //const data = JSON.parse(request_.response);
+//     //         if(request_.status ===200){
+//     //           
+//                 //const data = JSON.parse(request_.response);
 
-    //             console.log(data);
+//     //             console.log(data);
 
-    //             let creat_ul = document.createElement('ul');
+//     //             let creat_ul = document.createElement('ul');
 
-    //             for(let i = 0; i < data.length; i++) {
+//     //             for(let i = 0; i < data.length; i++) {
 
-    //                 creat_ul.innerHTML += `<li>
-    //                                         <a href="${data[i].href}">${data[i].text}</a>    
-    //                                         </li>`;
-    //             }
+//     //                 creat_ul.innerHTML += `<li>
+//     //                                         <a href="${data[i].href}">${data[i].text}</a>    
+//     //                                         </li>`;
+//     //             }
 
-    //             search_res.innerHTML = creat_ul.outerHTML;    
-    //         } else {
-    //             console.log("error");
-    //         }
-    //     });
+//     //             search_res.innerHTML = creat_ul.outerHTML;    
+//     //         } else {
+//     //             console.log("error");
+//     //         }
+//     //     });
 
-    });
+//     });
     
-// NPM - это облачный сервис.npm
+// // NPM - это облачный сервис.
     
 
 }());
